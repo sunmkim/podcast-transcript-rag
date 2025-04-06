d28df8968c6b35bb3c0ca8b6c5b4921e7ed6cbde
Sun Kim <sunkim924@gmail.com>
2025-04-06 09:45:11 -0400

    cleanup script

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # podcast-transcript-rag
 My RAG project
+
+To activate the development environment, run this line: `source .venv/bin/activate`
--- create_embeddings.py
+++ create_embeddings.py
@@ -3,7 +3,6 @@ from sentence_transformers import SentenceTransformer
 
 MODEL_NAME = "all-mpnet-base-v2"
 
-
 # read in data from json file
 with open('/workspaces/podcast-transcript-rag/data/data.json', 'rt') as f_in:
     docs_raw = json.load(f_in)
@@ -14,7 +13,7 @@ pretrained_model = SentenceTransformer(MODEL_NAME)
 # turn utterances into vector and store them
 data_with_vector = []
 for document in docs_raw:
-    document["vector"] = pretrained_model.encode(doc["utterance"]).tolist()
-    operations.append(doc)
+    document["vector"] = pretrained_model.encode(document["utterance"]).tolist()
+    data_with_vector.append(document)
 
-print(operations[1])
+print(data_with_vector[1])
